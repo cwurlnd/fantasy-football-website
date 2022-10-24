0d6e868ca40e5b43a55db5e5b4d999e141f7865b
cwurlnd <69475242+cwurlnd@users.noreply.github.com>
2022-10-24 11:13:32 -0400

    working league addition and list

--- my-app/src/Components/Create/Create.js
+++ my-app/src/Components/Create/Create.js
@@ -1,12 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { createLeague } from "../../Common/Services/Service";
 import Form from "../Form/Form";
 
 export default function Create() {
+  const [league, setLeague] = useState();
+  const [add, setAdd] = useState(false);
+
+  useEffect(() => {
+    // Check for add flag and make sure name state variable is defined
+    if (league && add) {
+      createLeague(league).then((newLeague) => {
+        setAdd(false);
+      });
+    }
+
+  }, [league, add]);
+
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    setAdd(true);
+  };
+
+  const onChangeHandler = (e) => {
+    e.preventDefault();
+    setLeague(e.target.value);
+  };
+
     return (
       <div>
       <section>
         <h1>Create a League</h1>
       </section>
-      <Form />
+      <Form onClick={onClickHandler} onChange={onChangeHandler}/>
       </div>
     );
   }
