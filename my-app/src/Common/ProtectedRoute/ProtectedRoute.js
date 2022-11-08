2d5b0690dbc3f50431e3ca71972af7f1ca9ffdc9
cwurlnd <69475242+cwurlnd@users.noreply.github.com>
2022-11-08 16:04:48 -0500

    auth redirect

--- my-app/src/Common/ProtectedRoute/ProtectedRoute.js
+++ my-app/src/Common/ProtectedRoute/ProtectedRoute.js
@@ -1,21 +1,13 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import { checkUser } from "../../Components/Auth/AuthService";
+import { Navigate } from "react-router-dom";
 
 // You can pass props using the spread operator to throw them on an object if there are too many to break out
 const ProtectedRoute = ({ element: Component, ...rest }) => {
-  const navigate = useNavigate();
-  const goBackHandler = () => {
-    navigate("/auth");
-  };
   if (checkUser()) {
     return <Component />;
   } else {
-    return (
-      <div>
-        <p>Unauthorized!</p> <button onClick={goBackHandler}>Please register or login.</button>
-      </div>
-    );
+    return <Navigate to="/auth" replace />;
   }
 };
 
